fca6bce33dd41e83507964f024935058a7e863aa
ILAKIYA <ilakiyaramukutty22@gmail.com>
2026-03-25 11:31:21 +0530

    Time: 7 ms (60.55%) | Memory: 47.7 MB (7.50%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : prerequisites){
+            adj.get(edge[1]).add(edge[0]);
+        }
+        int[] indegree=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int n:adj.get(i)){
+                indegree[n]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        ArrayList<Integer> res=new ArrayList<>();
+        int count=0;
+        while(!q.isEmpty()){
+            int vertex=q.poll();
+            count++;
+            res.add(vertex);
+            for(int neighbour : adj.get(vertex)){
+             indegree[neighbour]--;
+             if(indegree[neighbour]==0){
+                 q.add(neighbour);
+             }
+         }
+
+        }
+        return count==numCourses?true:false;
+        
+        
+    }
+}
