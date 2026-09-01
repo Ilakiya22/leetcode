7a81ef5a912becd315c70ae69f896d7c65e4deb0
ILAKIYA <ilakiyaramukutty22@gmail.com>
2026-09-01 11:39:30 +0530

    Time: 4 ms (100.00%) | Memory: 45.8 MB (68.15%) - LeetSync

---
+++ 2857-total-distance-traveled/total-distance-traveled.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+
+        int result = 0;
+
+        while (mainTank >= 5) {
+            mainTank -= 5;
+            result += 50;
+
+            if (additionalTank > 0) {
+                mainTank++;
+                additionalTank--;
+            }
+        }
+
+        result += mainTank * 10;
+
+        return result;
+    }
+}
